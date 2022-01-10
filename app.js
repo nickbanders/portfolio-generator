1edba00e7d58a6bf372592170ab39393d91aae75
nickbanders <nickbanders@gmail.com>
2022-01-10 02:22:15 -0600

    finished project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page_template');
+const { writeFile, copyFile } = require('./utils/generated-sites');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,27 +129,18 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-  
-  
-  
-//   promptUser()
-//   .then(answers => console.log(answers))
-//   .then(promptProject)
-//   .then(projectAnswers => console.log(projectAnswers));
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
